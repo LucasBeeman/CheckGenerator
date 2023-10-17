a3ddb33a5eb89972555346baeda9facdfb48427e
Lucas Beeman <70167892+LucasBeeman@users.noreply.github.com>
2023-10-17 00:03:33 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -13,16 +13,16 @@ using namespace std;
 
 const string FILE_NAME = "names.txt";
 //The constant arrays for spoken english are written here, since the arrays should be initialized only once.
+/*
 const string spokenOnes[10] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", ""};
 const string spokenTeens[10] = {"eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", ""};
 const string spokenTens[11] = {"ten", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety", ""};
 const string spokenPlaces[3] = {"hundred", "thousand", ""};
-
+*/
 void getNames(string fileName, vector<string> &nameList);
-void displayChecks(int iterator);
 string generateDate();
 double generateAmmount();
-string spokenNumber(double ammount);
+//string spokenNumber(double ammount);
 
 default_random_engine generator(time(0));
 
@@ -31,15 +31,11 @@ int main(){
   getNames(FILE_NAME, nameList);
   for (int i = 0; i < nameList.size(); i++) {
   cout << "----------------\n" <<
-    "Check Number: " << i << endl << "          " << "Date:" << generateDate() << endl <<
+    "Check Number: " << i << "                      " << "Date:" << generateDate() << endl <<
     "Pay to the Order of: " << nameList[i] << "          $" << FIXED_FLOAT(generateAmmount()) << endl <<
     "Memo Paycheck" << "                             " << "Signature: Lucas Beeman\n" <<
     "----------------\n\n";
-     
-
   }
-
- 
   return 0;
 }
 
@@ -105,7 +101,7 @@ string generateDate() { //choose a random date out of the 365 days of the year
   return month + '/' + day + "/" + year;
 }
 
-double generateAmmount() {
+double generateAmmount() { //choose a double between .01 and 10000.00
   uniform_real_distribution<double> distribution(0.01, 10000.00);
   return distribution(generator);
 }
